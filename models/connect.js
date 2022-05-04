3f9483eaefee69fb39069e372dd768cdc8c008ed
João Carvalho <joaove>
2022-05-04 11:31:44 +0100

    push

--- models/connect.js
+++ models/connect.js
@@ -2,16 +2,5 @@ const MongoClient = require('mongodb');
 
 const url = "mongodb+srv://JoaoIMO:imodream2022@cluster0.lyfie.mongodb.net/IMODREAM?retryWrites=true&w=majority"
 
-console.log("connectionString = " + url);
 
-const Url = MongoClient;
-const url = new Url({
-  url,
-  max: 10,
-  ssl: {
-    require: true,
-    rejectUnauthorized: false
-  }
-})
-
-module.exports = pool;
+module.exports = url;
